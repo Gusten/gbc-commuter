bfb3e50f0ceda0985675b6e9b88e307536be435e
Gusten <johan@insertcoin.se>
2017-10-12 16:32:38 +0200

    Added screenReceiver to manifest since it seemed to bug out on some devices

--- app/src/main/java/pub/gusten/gbgcommuter/MainActivity.java
+++ app/src/main/java/pub/gusten/gbgcommuter/MainActivity.java
@@ -11,11 +11,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
