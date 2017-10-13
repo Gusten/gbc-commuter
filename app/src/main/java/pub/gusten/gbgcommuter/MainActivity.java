521e1ebd905114c05e99cacc22e3fcba2739552a
Gusten <johan@insertcoin.se>
2017-10-13 10:02:40 +0200

    Fixed icon being incorrectly named in android os

--- app/src/main/java/pub/gusten/gbgcommuter/MainActivity.java
+++ app/src/main/java/pub/gusten/gbgcommuter/MainActivity.java
@@ -91,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(R.string.main_toolbar);
 
          filteredDepartures = new ArrayList<>();
 
